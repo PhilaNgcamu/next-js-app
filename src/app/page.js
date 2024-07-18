5e9bac19aeefbbaa1c86dad4c00721328ac7a206
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-18 09:01:54 +0200

    Style the Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import { FaSearch } from "react-icons/fa";
+import { MdSearch } from "react-icons/md";
 
 export default function Home() {
   return (
-    <div className="flex flex-row justify-between items-center p-5 rounded-lg background-custom-color w-4/5">
-      <h1 className="text-white text-2xl">Dashboard</h1>
-      <div className="flex items-center rounded-xl background-color-search-bar p-3.5">
-        <FaSearch className="text-white" />
+    <div className="flex flex-row justify-between items-center p-5 rounded-lg custom-background-color w-4/5">
+      <h1 className="custom-text-color font-bold">Dashboard</h1>
+      <div className="flex items-center rounded-xl background-color-search-bar p-2.5">
+        <MdSearch className="text-white" />
         <input
           type="text"
           placeholder="Search..."
