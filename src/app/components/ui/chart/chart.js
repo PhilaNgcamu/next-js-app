1299afd55fb761b34ff694abfe110c5f18e53972
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-18 16:56:47 +0200

    Master CSS

---
+++ src/app/components/ui/chart/chart.js
@@ -0,0 +1,87 @@
+"use client";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+export default function Chart() {
+  const data = [
+    {
+      name: "Sun",
+      visit: 4000,
+      click: 2400,
+    },
+    {
+      name: "Mon",
+      visit: 3000,
+      click: 1398,
+    },
+    {
+      name: "Tue",
+      visit: 2000,
+      click: 3800,
+    },
+    {
+      name: "Wed",
+      visit: 2780,
+      click: 3908,
+    },
+    {
+      name: "Thu",
+      visit: 1890,
+      click: 4800,
+    },
+    {
+      name: "Fri",
+      visit: 2390,
+      click: 3800,
+    },
+    {
+      name: "Sat",
+      visit: 3490,
+      click: 4300,
+    },
+  ];
+  return (
+    <div className="container h-[500px] mt-10 bg-[#182237] p-5 rounded-xl">
+      <h2 className="text-[#b7bac1] p-5">Weekly Recap</h2>
+      <ResponsiveContainer width="100%" height="85%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip contentStyle={{ background: "#ffffff", border: "none" }} />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="visit"
+            stroke="#8884d8"
+            strokeDasharray="5 5"
+          />
+          <Line
+            type="monotone"
+            dataKey="click"
+            stroke="#82ca9d"
+            strokeDasharray="3 4 5 2"
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,17 @@
 import Card from "../components/ui/card/card";
+import Chart from "../components/ui/chart/chart";
 
 const Dashboard = () => {
   return (
-    <div className="flex gap-10 mt-10">
-      <Card />
-      <Card />
-      <Card />
+    <div className="flex flex-col">
+      <div className="flex flex-row gap-10 mt-10">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div>
+        <Chart />
+      </div>
     </div>
   );
 };
