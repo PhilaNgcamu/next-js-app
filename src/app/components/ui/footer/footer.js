982433a5d216e2ef9d5e289a68bbb7909b5422d1
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-18 16:06:28 +0200

    Footer

--- src/app/components/ui/footer/footer.js
+++ src/app/components/ui/footer/footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <div className="flex p-7 justify-between items-center text-[#b7bac1] rounded-xl">
-      <span>Philasande Ngcamu</span>
-      <span>© All rights reserved</span>
+      <span className="font-bold">Philasande Ngcamu</span>
+      <span className="text-xs">© All rights reserved</span>
     </div>
   );
 }
