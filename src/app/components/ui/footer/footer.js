e704da983e220ef2cb7f13222f5dcebd262133c8
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-18 15:39:27 +0200

    Improved CSS

--- src/app/components/ui/footer/footer.js
+++ src/app/components/ui/footer/footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <div className="flex p-10 justify-between items-center text-[#b7bac1] rounded-xl">
-      <span className="relative right-3">Philasande Ngcamu</span>
-      <span className="relative left-3">© All rights reserved</span>
+    <div className="flex p-7 justify-between items-center text-[#b7bac1] rounded-xl">
+      <span>Philasande Ngcamu</span>
+      <span>© All rights reserved</span>
     </div>
   );
 }
