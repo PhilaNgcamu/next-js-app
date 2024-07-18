b7a5a336daca4e29396cf1923e990e44af4b5e87
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-18 09:29:09 +0200

    Navbar completed

---
+++ src/app/components/ui/navbar/navbar.js
@@ -0,0 +1,29 @@
+import {
+  MdSearch,
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+} from "react-icons/md";
+
+export default function Navbar() {
+  return (
+    <div className="flex flex-row justify-between items-center p-7 rounded-xl custom-background-color w-4/5">
+      <h1 className="custom-text-color font-bold">Dashboard</h1>
+      <div className="flex gap-2.5 p-0 h-11 items-center">
+        <div className="flex items-center p-2.5 rounded-xl background-color-search-bar gap-2.5 ">
+          <MdSearch className="text-white" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent outline-none border-0 text-white"
+          ></input>
+        </div>
+        <div className="text-white">
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,5 @@
-import { MdSearch } from "react-icons/md";
+import Navbar from "./components/ui/navbar/navbar";
 
 export default function Home() {
-  return (
-    <div className="flex flex-row justify-between items-center p-5 rounded-lg custom-background-color w-4/5">
-      <h1 className="custom-text-color font-bold">Dashboard</h1>
-      <div className="flex items-center rounded-xl background-color-search-bar p-2.5">
-        <MdSearch className="text-white" />
-        <input
-          type="text"
-          placeholder="Search..."
-          className="bg-transparent outline-none ml-3 text-white"
-        ></input>
-      </div>
-    </div>
-  );
+  return <Navbar />;
 }
