a46da73a697b8ee513f2ffacdfe682ad0e82bc4c
PhilaNgcamu <philangcamu17@gmail.com>
2024-07-29 16:59:40 +0200

    Display full informtion

--- src/app/dashboard/users/[id]/page.js
+++ src/app/dashboard/users/[id]/page.js
@@ -1,10 +1,35 @@
+import Link from "next/link";
+
 export default function SingleUserPage({ searchParams }) {
   console.log(searchParams);
   return (
-    <div>
-      <h1 className="text-3xl text-white">
-        Single User Page: {searchParams.name}
-      </h1>
+    <div className="p-10 bg-[#182237] flex flex-col gap-10 items-center">
+      <h1 className="text-2xl font-bold">{searchParams.name}</h1>
+      <p className="text-lg">Email: {searchParams.email}</p>
+      <p>Created At: {searchParams.createdAt}</p>
+      <p>Role: {searchParams.role}</p>
+      <p>Status: {searchParams.status}</p>
+      <img
+        src={searchParams.avatar}
+        alt={searchParams.name}
+        width={100}
+        height={100}
+        className="rounded-full"
+      />
+
+      <div>
+        <Link href="/dashboard/users">Back</Link>
+      </div>
+
+      <div>
+        <Link href={`/dashboard/users/${searchParams.id}/posts`}>Posts</Link>
+      </div>
+
+      <div>
+        <Link href={`/dashboard/users/${searchParams.id}/comments`}>
+          Comments
+        </Link>
+      </div>
     </div>
   );
 }
